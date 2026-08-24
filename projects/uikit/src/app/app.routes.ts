fae8419d940cf15a52dcb93e4989072f54f0b582
Honchar Denys <crackeraki@gmail.com>
2026-08-25 01:06:32 +0300

    uikit: add 10 more Form demo pages (radiobutton, rating, slider, textarea, togglebutton, toggleswitch, floatlabel, inputnumber, password, selectbutton)

--- projects/uikit/src/app/app.routes.ts
+++ projects/uikit/src/app/app.routes.ts
@@ -20,6 +20,51 @@ export const routes: Routes = [
 				loadComponent: () => import('./pages/form/checkbox/checkbox-demo').then((m) => m.CheckboxDemo),
 			},
 			{
+				path: 'form/radiobutton',
+				loadComponent: () =>
+					import('./pages/form/radiobutton/radiobutton-demo').then((m) => m.RadioButtonDemo),
+			},
+			{
+				path: 'form/rating',
+				loadComponent: () => import('./pages/form/rating/rating-demo').then((m) => m.RatingDemo),
+			},
+			{
+				path: 'form/slider',
+				loadComponent: () => import('./pages/form/slider/slider-demo').then((m) => m.SliderDemo),
+			},
+			{
+				path: 'form/textarea',
+				loadComponent: () => import('./pages/form/textarea/textarea-demo').then((m) => m.TextareaDemo),
+			},
+			{
+				path: 'form/togglebutton',
+				loadComponent: () =>
+					import('./pages/form/togglebutton/togglebutton-demo').then((m) => m.ToggleButtonDemo),
+			},
+			{
+				path: 'form/toggleswitch',
+				loadComponent: () =>
+					import('./pages/form/toggleswitch/toggleswitch-demo').then((m) => m.ToggleSwitchDemo),
+			},
+			{
+				path: 'form/floatlabel',
+				loadComponent: () => import('./pages/form/floatlabel/floatlabel-demo').then((m) => m.FloatLabelDemo),
+			},
+			{
+				path: 'form/inputnumber',
+				loadComponent: () =>
+					import('./pages/form/inputnumber/inputnumber-demo').then((m) => m.InputNumberDemo),
+			},
+			{
+				path: 'form/password',
+				loadComponent: () => import('./pages/form/password/password-demo').then((m) => m.PasswordDemo),
+			},
+			{
+				path: 'form/selectbutton',
+				loadComponent: () =>
+					import('./pages/form/selectbutton/selectbutton-demo').then((m) => m.SelectButtonDemo),
+			},
+			{
 				path: 'button/button',
 				loadComponent: () => import('./pages/button/button/button-demo').then((m) => m.ButtonDemo),
 			},
